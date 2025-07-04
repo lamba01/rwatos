873b2e048ad1003b8e5654d4b33df471c6e9df3b
lamba01 <jamesspaul987@gmail.com>
2025-07-05 00:16:55 +0100

    Refactor footer and navbar components: update layout for responsiveness, enhance logo integration, and add email link for contact.

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
       className="bg-[#061936] text-white pt-8 pb-2 px-6 mt-12"
       id="footer"
     >
-      <main className="flex justify-between">
+      <main className="flex flex-col sm:flex-row gap-5 justify-between">
         <div className="max-w-6xl flex items-center gap-5 space-y-2">
           <img src={footerlogo} alt="Rwatos logo" className="w-20" />
           <div>
@@ -21,7 +21,8 @@ function Footer() {
             Works, 4, Roach Road, Bow, London, E3 2PF
           </p>
           <p className="text-white/50">
-            <strong className="text-white">Email :</strong> info@rwatosltd.com
+            <strong className="text-white">Email :</strong>
+            <a href="mailto:info@rwatosltd.com"> info@rwatosltd.com</a>
           </p>
           <p className="text-white/50">
             <strong className="text-white">Phone :</strong>
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,10 +1,12 @@
-// import logo from "../assets/rwatoslogo.png";
+import logo from "../assets/rwatoslogo.png";
 export default function navBar() {
   return (
     <nav className="flex w-full justify-between items-center px-5 sm:px-10 z-20 fixed top-0 backdrop-blur-3xl h-[50px]">
       <div className="flex items-center">
-        {/* <img className="h-[50px] w-[50px] shadow-md" src={logo} alt="" /> */}
-        <h3 className=" font-bold text-xl text-[#2F6D9A]">RWATOS LTD</h3>
+        <img className="h-[50px] w-[50px]" src={logo} alt="" />
+        <h3 className=" font-bold text-xl text-blue-900 hidden sm:block">
+          RWATOS LTD
+        </h3>
       </div>
       <div className="flex gap-5 md:gap-10 capitalize">
         <a
