b3a4dcadd1ef4df829ab6732faa2781d5c522504
lamba01 <jamesspaul987@gmail.com>
2025-07-04 23:16:51 +0100

    Refactor footer component layout and update contact information; modify index.html for SEO optimization

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,21 +4,31 @@ import footerlogo from "../assets/IMG_1359.jpg";
 function Footer() {
   return (
     <footer className="bg-[#061936] text-white pt-8 pb-2 px-6 mt-12">
-      <div className="max-w-6xl flex items-center gap-5 space-y-2">
-        <img src={footerlogo} alt="Rwatos logo" className="w-20" />
-        <div>
-          <h4 className="text-xl font-semibold">Rwatos LTD</h4>
-          <p>Unit 15, Omega Works, 4, Roach Road, Bow, London, E3 2PF</p>
+      <main className="flex justify-between">
+        <div className="max-w-6xl flex items-center gap-5 space-y-2">
+          <img src={footerlogo} alt="Rwatos logo" className="w-20" />
+          <div>
+            <h4 className="text-xl font-semibold">Rwatos LTD</h4>
+            <p className="italic">Where Interior Vision Meets Trade Value.</p>
+          </div>
         </div>
-      </div>
-      <p>Email: info@rwatosltd.com</p>
-      <p>
-        Phone:{" "}
-        <a href="tel:+447722133636" className="text-white underline">
-          +44 07722 133636
-        </a>
-      </p>
-
+        <div>
+          <p className="text-white/50">
+            <strong className="text-white">Address :</strong> Unit 15, Omega
+            Works, 4, Roach Road, Bow, London, E3 2PF
+          </p>
+          <p className="text-white/50">
+            <strong className="text-white">Email :</strong> info@rwatosltd.com
+          </p>
+          <p className="text-white/50">
+            <strong className="text-white">Phone :</strong>
+            <a href="tel:+447722133636" className="underline">
+              {" "}
+              +44 07722 133636
+            </a>
+          </p>
+        </div>
+      </main>
       <div className="border-t border-gray-300 w-full mt-4"></div>
       <div className="w-full text-center pt-2 text-sm text-gray-500">
         &copy; {new Date().getFullYear()} Rwatos LTD. All rights reserved.
